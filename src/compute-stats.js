6d0a7f992eed59c5f5dbb31d9503401c5b85d676
Oleg SKLYANCHUK <sklyanchuk@gmail.com>
2016-02-17 16:42:37 +0100

    Avoid altering the default global result.
    This ensures the immutability of the default
    global result object and allows to run the
    code multiple times providing correct results.

--- src/compute-stats.js
+++ src/compute-stats.js
@@ -1,18 +1,32 @@
 import {tags, metrics} from 'adana-analyze';
 
-const emptyResult = {
-  statement: {passed: 0, total: 0},
-  branch: {passed: 0, total: 0},
-  line: {passed: 0, total: 0},
-  function: {passed: 0, total: 0},
-};
+/**
+ * Code coverage tags used in the report.
+ * @type {Array}
+ */
+const tagsToSelect = [
+  'statement',
+  'branch',
+  'line',
+  'function',
+];
 
-const usedTags = Object.keys(emptyResult);
+/**
+ * Generates an empty result.
+ * @param   {Array}  tagNameList Tag names.
+ * @returns {Object}             Empty result with default values.
+ */
+function createEmptyResult(tagNameList) {
+  return tagNameList.reduce((result, tagName) => {
+    result[tagName] = {passed: 0, total: 0};
+    return result;
+  }, {});
+}
 
 export default function computeStats(coverage) {
   function filesReducer(result, fileName) {
     const {locations} = coverage[fileName];
-    const tagStats = tags(locations, usedTags);
+    const tagStats = tags(locations, tagsToSelect);
     result.files[fileName] = {};
     Object.keys(tagStats).forEach(tagName => {
       const tagData = tagStats[tagName];
@@ -25,7 +39,7 @@ export default function computeStats(coverage) {
   }
 
   return Object.keys(coverage).reduce(filesReducer, {
-    global: emptyResult,
+    global: createEmptyResult(tagsToSelect),
     files: {},
   });
 }
