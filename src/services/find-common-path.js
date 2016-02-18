091111a7861fbca74f45109a6340f2ceff26210b
Oleg SKLYANCHUK <sklyanchuk@gmail.com>
2016-02-18 20:24:54 +0100

    Refactor, document and test find-common-path.

--- src/services/find-common-path.js
+++ src/services/find-common-path.js
@@ -1,3 +1,14 @@
+/**
+ * Common filepath finder.
+ * @module services/find-common-path
+ */
+
+/**
+ * Common path reducer for use in Array.prototype.reduce.
+ * @param   {Array<String>} a File path, e.g. `['src', 'foo.js'];`.
+ * @param   {Array<String>} b File path, e.g. `['src', 'bar.js'];`.
+ * @returns {Array<String>}   Common path, e.g. `['src']`.
+ */
 function commonPathReducer(a, b) {
   const result = [];
   const minLength = Math.min(a.length, b.length);
@@ -8,10 +19,16 @@ function commonPathReducer(a, b) {
   return result;
 }
 
-export default function findCommonPath(filePaths = []) {
+/**
+ * Determines a common root path in the list of file paths.
+ * @param   {Array<String>} filePaths List of paths.
+ * @returns {String}                  Common root path.
+ * @example
+ *  findCommonPath(['src/foo.js', 'src/bar.js']); // 'src'
+ */
+export default function findCommonPath(filePaths) {
   if (!filePaths.length) { return ''; }
-  const splitFullFilePaths = filePaths
-    .map(filePath => filePath.split('/'));
+  const splitFullFilePaths = filePaths.map(filePath => filePath.split('/'));
   const commonPath = splitFullFilePaths.length === 1 ?
     splitFullFilePaths[0].slice(0, -1) :
     splitFullFilePaths.reduce(commonPathReducer);
